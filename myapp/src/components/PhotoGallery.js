334da3a3c272878a420a6be549e5afda39902812
Swamy <swamykumar539@gmail.com>
2022-10-31 16:17:50 +0530

    test

--- myapp/src/components/PhotoGallery.js
+++ myapp/src/components/PhotoGallery.js
@@ -2,31 +2,31 @@ import React, { useState, useEffect } from "react";
 import "./PhotoGallery.css";
 
 const PhotoGallery = () => {
-  const [imagesList, setImagesList] = useState([]);
+  const [result, setResult] = useState([]);
 
   useEffect(() => {
-    const task = async () => {
+    const fetching = async () => {
       const res = await fetch(
         "https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=f9736f4d370f9c7115a952951b506569&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1"
       );
       const data = await res.json();
       console.log(data.photos.photo);
-      setImagesList(data.photos.photo);
+      setResult(data.photos.photo);
     };
-    task();
+    fetching();
   }, []);
 
   return (
-    <div className="app-container">
+    <div className="container">
       <h1 className="heading">Images</h1>
-      <div className="images-container">
-        {imagesList.map((each, i) => {
+      <div className="Gallery-container">
+        {result.map((each, i) => {
           return (
             <div className="image-container" key={i}>
               <img
                 src={`https://farm${each.farm}.staticflickr.com/${each.server}/${each.id}_${each.secret}.jpg`}
                 className="image"
-                alt="shopping-images"
+                alt="Gallery-images"
               />
               <p className="title">{each.title}</p>
             </div>
