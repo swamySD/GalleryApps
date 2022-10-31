f619e1b8aca476abc98730ffc6891b2eea3c8d42
Swamy <swamykumar539@gmail.com>
2022-10-31 14:52:58 +0530

    test

--- myapp/src/components/PhotoGallery.js
+++ myapp/src/components/PhotoGallery.js
@@ -1,43 +1,40 @@
-import React, { useEffect, useState } from 'react'
-import classes from './PhotoGallery.module.css'
+import React, { useState, useEffect } from "react";
+import "./PhotoGallery.css";
 
 const PhotoGallery = () => {
-    const [result,setResult]=useState([])
+  const [imagesList, setImagesList] = useState([]);
 
-useEffect(() => {
-      const url = "https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=f9736f4d370f9c7115a952951b506569&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1";
-
-      const fetchData = async () => {
-          try {
-              const response = await fetch(url);
-              const data = await response.json();
-              console.log(data)
-              console.log(data.photos.photo);
-              setResult(data.photos.photo)
-          } catch (error) {
-            console.log("error", error);
-          }
-      };
-fetchData();
-    }, []);
+  useEffect(() => {
+    const task = async () => {
+      const res = await fetch(
+        "https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=f9736f4d370f9c7115a952951b506569&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1"
+      );
+      const data = await res.json();
+      console.log(data.photos.photo);
+      setImagesList(data.photos.photo);
+    };
+    task();
+  }, []);
 
   return (
-    <div className={classes.container}>
-      
-       
-       {result.length>1&&
-        result.map(each=>{
-          return(
-            <div key={each.id} className={classes.innercontainer}>
-              <img className={classes.image} src={`https://farm${each.farm}.staticflickr.com/${each.server}/${each.id}_${each.secret}.jpg`} alt="photoss" />
-              <p className={classes.title}>{each.title}</p>
-              </div>
-          )
-        })
-      }
-      
+    <div className="app-container">
+      <h1 className="heading">Images</h1>
+      <div className="images-container">
+        {imagesList.map((each, i) => {
+          return (
+            <div className="image-container" key={i}>
+              <img
+                src={`https://farm${each.farm}.staticflickr.com/${each.server}/${each.id}_${each.secret}.jpg`}
+                className="image"
+                alt="shopping-images"
+              />
+              <p className="title">{each.title}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PhotoGallery
+export default PhotoGallery;
