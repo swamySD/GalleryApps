65268d1cadcf7649d412903f8d78a8a3510373e4
Swamy <swamykumar539@gmail.com>
2022-11-10 17:17:47 +0530

    test

--- myapp/src/components/PhotoGallery.js
+++ myapp/src/components/PhotoGallery.js
@@ -1,25 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useRef } from "react";
 import "./PhotoGallery.css";
 
 const PhotoGallery = () => {
   const [result, setResult] = useState([]);
+  let shouldlog = useRef(true);
 
-  useEffect(() => {
-    const fetching = async () => {
-      const res = await fetch(
-        "https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=f9736f4d370f9c7115a952951b506569&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1"
-      );
-      const data = await res.json();
-      console.log(data.photos.photo);
-      setResult(data.photos.photo);
-    };
-    fetching();
-  }, []);
+ 
+
+
+ const fetching = async () => {
+  const res = await fetch(
+    "https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=f9736f4d370f9c7115a952951b506569&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1"
+  );
+  const data = await res.json();
+  console.log(data.photos.photo);
+  setResult(data.photos.photo);
+};
+
+
+useEffect(() => {
+ 
+  if (shouldlog.current) {
+   shouldlog.current = false;
+   console.log("component mounted");
+   
+   fetching();
+   }
+ 
+ return () => console.log("function cleaned up");
+ }, []);
+
+
+ 
 
   return (
     <div className="container">
-      <h1 className="heading">Images</h1>
-      <div className="Gallery-container">
+     <div className="Gallery-container">
         {result.map((each, i) => {
           return (
             <div className="image-container" key={i}>
