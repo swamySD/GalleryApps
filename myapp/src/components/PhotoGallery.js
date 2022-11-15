f64d42f85cc8681a5ce7738436d0d4d078a16f68
Swamy <swamykumar539@gmail.com>
2022-11-15 12:22:14 +0530

    "modal"

--- myapp/src/components/PhotoGallery.js
+++ myapp/src/components/PhotoGallery.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect,useRef } from "react";
+
+import Modal from 'react-bootstrap/Modal';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./PhotoGallery.css";
 
 const PhotoGallery = () => {
   const [result, setResult] = useState([]);
+  const [show, setShow] = useState(false);
   let shouldlog = useRef(true);
 
  
@@ -15,7 +19,12 @@ const PhotoGallery = () => {
   const data = await res.json();
   console.log(data.photos.photo);
   setResult(data.photos.photo);
+  
 };
+
+
+
+
 
 
 useEffect(() => {
@@ -30,23 +39,61 @@ useEffect(() => {
  return () => console.log("function cleaned up");
  }, []);
 
+ const showImageModal=(image,i)=>{
+ console.log(image,i)
+ setShow({image})
+ }
 
+let date=new Date()
  
 
   return (
     <div className="container">
+
+    {show.image&&
+    <>
+
+<Modal
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+           
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img src={show.image} alt="" className="modalImage"/>
+        </Modal.Body>
+      </Modal>
+
+
+    </>
+    }
+
      <div className="Gallery-container">
+
         {result.map((each, i) => {
+          const img=`https://farm${each.farm}.staticflickr.com/${each.server}/${each.id}_${each.secret}.jpg`
           return (
             <div className="image-container" key={i}>
+              <div className="container-1">
               <img
-                src={`https://farm${each.farm}.staticflickr.com/${each.server}/${each.id}_${each.secret}.jpg`}
+                src={img}
                 className="image"
                 alt="Gallery-images"
+                onClick={()=>showImageModal(img,i)}
               />
+              </div>
+              <div className="container-2">
               <p className="title">{each.title}</p>
+              <p className="title">{`${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`}</p>
+            </div>
             </div>
           );
+
         })}
       </div>
     </div>
