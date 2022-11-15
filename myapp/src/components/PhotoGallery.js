b61c36d6cc5184d649c3cff2658d86f36a93220a
ambikav98 <ambikav600@gamil.com>
2022-11-15 14:42:40 +0530

    final commit

--- myapp/src/components/PhotoGallery.js
+++ myapp/src/components/PhotoGallery.js
@@ -1,50 +1,69 @@
-import React, { useState, useEffect,useRef } from "react";
+import React, { useState, useEffect } from "react";
+import Modal from 'react-bootstrap/Modal';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./PhotoGallery.css";
 
 const PhotoGallery = () => {
-  const [result, setResult] = useState([]);
-  let shouldlog = useRef(true);
+  const [imagesList, setImagesList] = useState([]);
+  const [show, setShow] = useState(false);
 
- 
+  useEffect(() => {
+    const task = async () => {
+      const res = await fetch(
+        "https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=f9736f4d370f9c7115a952951b506569&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1"
+      );
+      const data = await res.json();
+      console.log(data.photos.photo);
+      setImagesList(data.photos.photo);
+    };
+    task()
+  }, []);
 
-
- const fetching = async () => {
-  const res = await fetch(
-    "https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=f9736f4d370f9c7115a952951b506569&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1"
-  );
-  const data = await res.json();
-  console.log(data.photos.photo);
-  setResult(data.photos.photo);
-};
-
-
-useEffect(() => {
- 
-  if (shouldlog.current) {
-   shouldlog.current = false;
-   console.log("component mounted");
-   
-   fetching();
-   }
- 
- return () => console.log("function cleaned up");
- }, []);
-
-
- 
+  const showImageModal=(image,i)=>{
+    console.log(image,i)
+    setShow({image})
+    }
+let myCurrentDate = new Date()
+let date = myCurrentDate.getDate();
+let month = myCurrentDate.getMonth() + 1;
+let year = myCurrentDate.getFullYear();
 
   return (
-    <div className="container">
-     <div className="Gallery-container">
-        {result.map((each, i) => {
+    <div className="app-container">
+      {show.image&&
+    <>
+  <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+           </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img src={show.image} alt="" className="modalImage"/>
+        </Modal.Body>
+      </Modal>
+  </>
+    }
+      
+      <div className="images-container">
+        {imagesList.map((each, i) => {
+          const img=`https://farm${each.farm}.staticflickr.com/${each.server}/${each.id}_${each.secret}.jpg`
           return (
             <div className="image-container" key={i}>
               <img
-                src={`https://farm${each.farm}.staticflickr.com/${each.server}/${each.id}_${each.secret}.jpg`}
+                src={img}
                 className="image"
-                alt="Gallery-images"
+                onClick={()=>showImageModal(img,i)}
+                alt="shopping-images"
               />
-              <p className="title">{each.title}</p>
+              <div >
+              <p className="title">{each.title} </p>
+              <p className="date">{date-i}/{month<10?`0${month}`:`${month}`}/{year}</p>
+              </div>
             </div>
           );
         })}
